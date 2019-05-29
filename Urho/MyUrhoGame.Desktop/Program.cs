cb6bd4e00505b7be6f1f9e020d61971638d806ba
Tong Zhi <tong.zhi.1989@gmail.com>
2019-05-29 21:08:57 +0800

    更改 MyUrhoGame.Desktop 项目。

--- Urho/MyUrhoGame.Desktop/Program.cs
+++ Urho/MyUrhoGame.Desktop/Program.cs
@@ -1,13 +1,27 @@
-﻿using Urho;
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Urho;
 
 namespace MyUrhoGame.Desktop
 {
     class Program
     {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr LoadLibrary(string dllToLoad);
+
         static void Main(string[] args)
         {
-            new MyGame(new ApplicationOptions("MyData")).Run();
-            // For a console app Urho will create a Windows/macOS/Linux window using SDL 
+            var assmLocation = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+            var monoUrhoPath = Path.Combine(assmLocation, "Win64_OpenGL", "mono-urho.dll");
+            _ = LoadLibrary(monoUrhoPath);
+
+            var options = new ApplicationOptions()
+            {
+                ResourcePrefixPaths = new[] { assmLocation },
+                ResourcePaths = new[] { "MyData" }
+            };
+            new MyGame(options).Run();
         }
     }
 }
